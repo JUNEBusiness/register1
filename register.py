80099a8d4f1bf883e1e531e4d6b39d26a6c08a62
Omonigho Justice <103575069+JUNEBusiness@users.noreply.github.com>
2022-12-14 16:05:58 +0100

    created the api path using jsonify to return output

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, request, render_template, redirect
+from flask import Flask, flash, request, render_template, jsonify
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 import re
@@ -7,14 +7,15 @@ import re
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-        company_legal_name = request.form['companyLegalName']
-        company_name = request.form['companyName']
-        username = request.form['username']
-        email = request.form['email']
-        phone_number = request.form['phoneNumber']
-        password = request.form['password']
-        confrim_password = request.form['confrimPassword']
+    if request.method == 'POST' and 'username' in request.json and 'password' in request.json and 'email' in request.json :
+        content = request.json
+        company_legal_name = content['companyLegalName']
+        company_name = content['companyName']
+        username = content['username']
+        email = content['email']
+        phone_number = content['phoneNumber']
+        password = content['password']
+        confrim_password = content['confrimPassword']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE (username = % s OR email = % s OR phone_number = % s OR company_legal_name = % s', (username, email, phone_number, company_legal_name, ))
         account = cursor.fetchone()
@@ -28,19 +29,25 @@ def register():
             msg = 'Account already exists !'
         elif not re.fullmatch(regex, email):
             msg = 'Invalid email address !'
+            return jsonify({'errorMessage':msg})
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers !'
+            return jsonify({'errorMessage':msg})
         elif not re.match(r'[A-Za-z0-9]+', password) and len(password) < 6:
             msg = 'Pasword must at least 6, at least 1 character(s) and number(s)!'
+            return jsonify({'errorMessage':msg})
         elif not username or not password or not email:
             msg = 'Please fill out the form !'
+            return jsonify({'errorMessage':msg})
         elif password != confrim_password:
             msg = 'Password does not match confirmation'
+            return jsonify({'errorMessage':msg})
         else:
             password_hash = generate_password_hash(password)
             cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s, % s)', (company_legal_name, company_name, username, email, phone_number, password_hash, ))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
+            return jsonify({'errorMessage':msg})
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
-    return render_template('register.html', msg = msg)
+        return jsonify({'errorMessage':msg})
